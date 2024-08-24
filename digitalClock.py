3638d9701dd59db19b710200ace9f30d502b8639
siddhant <siddhantmishra2311@gmail.com>
2024-08-24 20:47:11 +0530

    digitalClock.py is edited

--- digitalClock.py
+++ digitalClock.py
@@ -3,4 +3,9 @@ from tkinter import Tk,Label
 window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
+window.configure(bg="steelblue")
+
+label = Label(window,text="welcome",font=("ariel Black",78,"bold"),bg="steelblue",fg="white")
+label.pack(pady=100)
+
 window.mainloop()
