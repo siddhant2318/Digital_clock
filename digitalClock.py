51344efc1133c0f3935f20d5bcb67dd9b6f19273
siddhant <siddhantmishra2311@gmail.com>
2024-08-25 00:09:01 +0530

    changed the color of BG to red

--- digitalClock.py
+++ digitalClock.py
@@ -5,7 +5,7 @@ window.title("Digital Clock")
 window.geometry("600x300")
 window.configure(bg="steelblue")
 
-label = Label(window,text="welcome",font=("ariel Black",78,"bold"),bg="steelblue",fg="white")
+label = Label(window,text="welcome",font=("ariel Black",78,"bold"),bg="red",fg="white")
 label.pack(pady=100)
 
 window.mainloop()
